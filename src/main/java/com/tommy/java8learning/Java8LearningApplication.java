d99e0ca8d198ea5b32534cbed2dff4e4b9eeaef1
LimHanGyeol <dlagksruf19@naver.com>
2020-10-27 05:20:34 +0900

    Initial commit

---
+++ src/main/java/com/tommy/java8learning/Java8LearningApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.java8learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8LearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8LearningApplication.class, args);
+    }
+
+}
