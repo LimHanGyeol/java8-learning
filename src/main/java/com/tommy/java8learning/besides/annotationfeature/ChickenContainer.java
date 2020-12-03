8a889de8cd6b5f39a6e9d3154ed4232f414761a2
LimHanGyeol <dlagksruf19@naver.com>
2020-12-03 14:57:07 +0900

    [style] 어노테이션 추가 설명 주석 추가

--- src/main/java/com/tommy/java8learning/besides/annotationfeature/ChickenContainer.java
+++ src/main/java/com/tommy/java8learning/besides/annotationfeature/ChickenContainer.java
@@ -6,7 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 // 컨테이너가 가지고 있는 Retention 과 Target 정보는 반드시 자기가 감쌀 어노테이션보다 같거나 넓어야 한다.
-// 즉, ChickenContainer의 Retention 과 Target 의 범위는 @Chicken 보다 넓어야 한다.
+// 즉, ChickenContainer 의 Retention 과 Target 의 범위는 @Chicken 보다 넓어야 한다.
+// 추가적인 내용은 Effective Java 3/E 아이템 39에서 확인할 수 있다.
 @Target(ElementType.TYPE_USE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ChickenContainer {
